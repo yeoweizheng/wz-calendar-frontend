b3777f18418a6d917deea145f8d0862e561c12f1
Wei Zheng <wzyeo1990@gmail.com>
2023-05-23 16:05:45 +0100

    use toggle

--- src/components/TopAppBar.js
+++ src/components/TopAppBar.js
@@ -5,8 +5,7 @@ import Typography from '@mui/material/Typography';
 import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 import CalendarTodayIcon from '@mui/icons-material/CalendarToday';
-import Menu from '@mui/material/Menu';
-import MenuItem from '@mui/material/MenuItem';
+import CalendarViewMonthIcon from '@mui/icons-material/CalendarViewMonth'
 import { useAuth } from "../services/auth";
 import { useGlobalData } from '../services/globalData';
 
@@ -14,24 +13,15 @@ export default function TopAppBar() {
 
   const { isAuthenticated } = useAuth();
   const [globalData, setGlobalData] = useGlobalData();
-  const [calViewAnchorEl, setCalviewAnchorEl] = React.useState(null);
 
   const toggleSidebar = React.useCallback(() => {
     setGlobalData((prev) => ({...prev, sidebarOpen: !prev.sidebarOpen}))
   }, [setGlobalData])
 
-  const openCalViewMenu = React.useCallback((event) => {
-    setCalviewAnchorEl(event.currentTarget);
-  }, [setCalviewAnchorEl])
-
-  const closeCalViewMenu = React.useCallback(() => {
-    setCalviewAnchorEl(null);
-  }, [setCalviewAnchorEl])
-
-  const switchView = React.useCallback((view) => {
-    setGlobalData((prev) => ({...prev, calView: view}));
-    closeCalViewMenu();
-  }, [setGlobalData, closeCalViewMenu])
+  const toggleCalView = React.useCallback(() => {
+    let calView = globalData.calView === "weekly" ? "monthly" : "weekly";
+    setGlobalData((prev) => ({...prev, calView: calView}));
+  }, [setGlobalData, globalData.calView])
 
   return (
     <AppBar position="static">
@@ -43,18 +33,7 @@ export default function TopAppBar() {
         <Typography variant="h6" component="div" sx={{ flex: 1 }}> WZ Calendar </Typography>
         { isAuthenticated() ?
           <React.Fragment>
-            <IconButton color="inherit" onClick={openCalViewMenu}> <CalendarTodayIcon /> </IconButton>
-            <Menu
-              anchorEl={calViewAnchorEl}
-              anchorOrigin={{vertical: "top", horizontal: "right"}}
-              keepMounted
-              transformOrigin={{vertical: "top", horizontal: "right"}}
-              open={Boolean(calViewAnchorEl)}
-              onClose={closeCalViewMenu}
-            >
-              <MenuItem onClick={()=>switchView("weekly")} selected={globalData.calView === "weekly"}>Week</MenuItem>
-              <MenuItem onClick={()=>switchView("monthly")} selected={globalData.calView === "monthly"}>Month</MenuItem>
-            </Menu>
+            <IconButton color="inherit" onClick={toggleCalView}> {globalData.calView === "weekly" ? <CalendarTodayIcon /> : <CalendarViewMonthIcon />} </IconButton>
           </React.Fragment>
           : null
         }
